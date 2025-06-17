b4fadbd43b4978b67f6084a14c950969f86adb65
Anderson Brito de Lima <ablima.lec@sabesp.com.br>
2025-06-17 13:30:34 -0300

    Operadores

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,18 @@
+#adição 
+print(10 + 5)
+#subtração
+print(10 - 5)
+#multiplicação
+print(10 * 5)   
+#divisão
+print(10 / 5)
+#divisão inteira
+print(10 // 5)  
+#módulo - resto da divisão
+print(10 % 5)   
+#exponenciação - potência
+print(10 ** 5)
+
+# Precedência de operadores
+print(10 + 5 * 2)  # Multiplicação tem precedência sobre adição
+
---
+++ operadores_comparacao.py
@@ -0,0 +1,5 @@
+saldo = 200
+saque = 200
+
+print(saldo == saque)  # Igualdade
+print(saldo != saque)  # Diferença
