56b089fd3b4648213c91baf653a1af32d235d096
Anderson Brito de Lima <ablima.lec@sabesp.com.br>
2025-06-17 12:42:36 -0300

    first commit

---
+++ Funcoes_entrada_saida.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep=" - ", end=".\n")
---
+++ variaveis_constantes.py
@@ -0,0 +1,16 @@
+# Variáveis usando snake_case
+nome = "Anderson"
+idade = 28
+nome = "Roberto"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+limite_saque_diario = 500
+print(limite_saque_diario)
+
+
+# Constantes
+BRAZILIAN_STATES = ["SP", "RJ", "MG", "RS"]
+
+print(BRAZILIAN_STATES)
