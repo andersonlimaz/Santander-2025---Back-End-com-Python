4306cbe15f0f84f21c4fff89a4969d210afed2b2
Anderson Brito de Lima <ablima.lec@sabesp.com.br>
2025-06-18 16:05:20 -0300

    indentacao_bloca

---
+++ Identacao_blocos/indetacao_blocos.py
@@ -0,0 +1,16 @@
+
+
+def sacar(valor):
+    saque = 500
+
+    if saque >= valor:
+        print(f"valor sacado!")
+        print("retire o seu dinheiro na boca do caixa.")
+
+    print("obrigador por ser nosso cliente, tenha um bom dia!")
+
+def depositar(valor):
+    saldo = 500 
+    saldo += valor
+
+sacar(500)
