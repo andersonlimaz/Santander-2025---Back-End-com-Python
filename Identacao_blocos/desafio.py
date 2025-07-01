aee9ecea4eeba8c5aca36b9d96408c4639bf6305
Anderson Brito de Lima <ablima.lec@sabesp.com.br>
2025-07-01 10:05:11 -0300

    Desafio Pronto

--- Identacao_blocos/desafio.py
+++ Identacao_blocos/desafio.py
@@ -7,10 +7,10 @@ escolheu a linguagem Python. Para a primeira versão do sistema devemos implemen
 
 menu = """
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sai
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[4] Sai
 
 =>"""
 
@@ -24,15 +24,15 @@ while True:
 
     opcao = input(menu)
     
-    if opcao =="d":
-        valor = float(input("Informe o valor do depósito:"))
+    if opcao =="1":
+        valor = float(input("Informe o valor do depósito: R$"))
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
         else:
             print('Operação falhou! O valor informado é invalido')
     
-    elif opcao =="s":
+    elif opcao =="2":
         valor = float(input("Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo
@@ -55,16 +55,16 @@ while True:
             extrato += f"Saque: R$ {valor:.2f}\n"
 
         else: 
-            print("Operação falhou! O valor informadoé inválido.")
+            print("Operação falhou! O valor informado não é inválido.")
 
 
-    elif opcao =="e":
-        print("\n =========================== EXTRATO =======================")
+    elif opcao =="3":
+        print("\n ***************** EXTRATO ******************")
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R${saldo:.2f}")
-        print("==============================================================")
+        print("*************************************************")
     
-    elif opcao == "q":
+    elif opcao == "4":
         break
 
     else:
